18c6dbf9808225ab600d2b051c5aefdd1f7df0b6
xwhoknowz <jeremiah2498@gmail.com>
2023-12-21 20:49:45 -0600

    added our if and else if conditionals

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,12 +4,12 @@ var generateBtn = document.querySelector("#generate");
 
 
 
-function generatePassword(){
+function generatePassword() {
   // Below are confirm request that establish what types of characters the user wants in their password
-// We also have a prompt that requests input for character length
-// all responses are logged to the console
+  // We also have a prompt that requests input for character length
+  // all responses are logged to the console
   var answer1 = window.confirm("Do you want uppercase letters?");
-  
+
   console.log(answer1);
 
   var answer2 = window.confirm("Do you want lowercase letters?");
@@ -17,46 +17,169 @@ function generatePassword(){
   console.log(answer2);
 
   var answer3 = window.confirm("Do you want numbers?");
-  
+
   console.log(answer3);
-  
+
   var answer4 = window.confirm("Do you want special characters?");
-  
+
   console.log(answer4);
-  
+
   var charCount = window.prompt("How many characters do you need for your password?")
-  
+
   console.log(charCount);
-  
+
   //Below are our arrays the PC will use to create the password 
-  var alphaUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  
-  var alphaLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  
-  var number = ["1","2","3","4","5","6","7","8","9"];
-  
-  var specialChar =["@","(","~","!","@","#","$","%","^","&","*","_","-","+","=","`","(",")","{","}","[","]",":",";","'","<",">",",",".","?","/",")"];
+  var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+  var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+  var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  var specialChar = ["@", "(", "~", "!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "=", "`", "(", ")", "{", "}", "[", "]", ":", ";", "'", "<", ">", ",", ".", "?", "/", ")"];
 
   // below are the variable that will be used by the pc to select the radomized characters for the password
   // Math.random (selects a random number between 0 and 1)is being multiplied by the index length of each array and then Math.floor rounds them down to the nearest whole integer
-var aU = Math.floor(Math.random()*alphaUpper.length);
+  var aU = Math.floor(Math.random() * alphaUpper.length);
 
-console.log(aU);
+  console.log(aU);
 
-var aL = Math.floor(Math.random()*alphaLower.length);
+  var aL = Math.floor(Math.random() * alphaLower.length);
 
-console.log(aL);
+  console.log(aL);
 
-var nB = Math.floor(Math.random()*number.length);
+  var nB = Math.floor(Math.random() * number.length);
 
-console.log(nB);
+  console.log(nB);
 
-var sC = Math.floor(Math.random()*specialChar.length);
+  var sC = Math.floor(Math.random() * specialChar.length);
 
-console.log(sC);
+  console.log(sC);
 
+  // 
+
+  if (answer1 === true &&
+    answer2 === true &&
+    answer3 === true &&
+    answer4 === true
+  ) {
+
+
+
+  } else if (answer1 === false &&
+    answer2 === true &&
+    answer3 === true &&
+    answer4 === true
+  ) {
+
+
+
+  } else if (answer1 === true &&
+    answer2 === false &&
+    answer3 === true &&
+    answer4 === true
+  ) {
+
+    
+
+  } else if (answer1 === true &&
+    answer2 === true &&
+    answer3 === false &&
+    answer4 === true
+  ) {
+
+
+
+  } else if (answer1 === true &&
+    answer2 === true &&
+    answer3 === true &&
+    answer4 === false
+  ) {
+
+
+
+  } else if (answer1 === false &&
+    answer2 === false &&
+    answer3 === true &&
+    answer4 === true
+  ) {
+
+
+
+  } else if (answer1 === false &&
+    answer2 === true &&
+    answer3 === false &&
+    answer4 === true
+  ) {
+
+
+
+  } else if (answer1 === false &&
+    answer2 === true &&
+    answer3 === true &&
+    answer4 === false
+  ) {
+
+
+
+  } else if (answer1 === true &&
+    answer2 === false &&
+    answer3 === false &&
+    answer4 === true
+  ) {
+
+
+
+  } else if (answer1 === true &&
+    answer2 === false &&
+    answer3 === true &&
+    answer4 === false
+  ) {
+
+
+
+  }else if (answer1 === true &&
+    answer2 === true &&
+    answer3 === false &&
+    answer4 === false
+  ) {
+
+
+
+  } else if (answer1 === true &&
+    answer2 === false &&
+    answer3 === false &&
+    answer4 === false
+  ) {
+
+
+
+  } else if (answer1 === false &&
+    answer2 === true &&
+    answer3 === false &&
+    answer4 === false
+  ) {
+
+
+
+  } else if (answer1 === false &&
+    answer2 === false &&
+    answer3 === true &&
+    answer4 === false
+  ) {
+
+
+
+  } else if (answer1 === false &&
+    answer2 === false &&
+    answer3 === false &&
+    answer4 === true
+  ) {
+  } else {
+
+
+
+  }
 }
-
 generatePassword();
 
 
