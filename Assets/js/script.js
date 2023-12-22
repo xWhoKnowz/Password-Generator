dd70b2737fb52d49c689497a8c9c1114a19efbcd
xwhoknowz <jeremiah2498@gmail.com>
2023-12-21 18:12:05 -0600

    added arrays containing the 4 character types

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -28,7 +28,16 @@ function generatePassword(){
   
   console.log(charCount);
   
+  //Below are our arrays the PC will use to create the password 
+  var alphaUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  
+  var alphaLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  
+  var number = ["1","2","3","4","5","6","7","8","9"];
+  
+  var specialChar =["@","(","~","!","@","#","$","%","^","&","*","_","-","+","=","`","(",")","{","}","[","]",":",";","'","<",">",",",".","?","/",")"];
 
+ 
 }
 
 generatePassword();
